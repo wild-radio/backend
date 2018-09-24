b7b0b35a65435d9f26ad3ced8edf6e8cb8d33a2b
Renato Böhler <renato.bohler@gmail.com>
2018-09-24 20:50:58 -0300

    implementa endpoints de sistema

--- routes/impl/sistemaImpl.js
+++ routes/impl/sistemaImpl.js
@@ -1,11 +1,42 @@
-export const getSistemas = (req, res) => {
-  // TODO: implementação
-  res.status(200).send('GET sistemas');
+import db from '../../database/db';
+
+export const getSistemas = async (req, res) => {
+  const sistemas = await db.getResultList('select * from Sistema');
+
+  const sistemasCameras = await Promise.all(
+    sistemas.map(async sistema => {
+      const cameras = await db.getResultList(
+        `select * from Camera where idSistema = ${sistema.id}`,
+      );
+      return { ...sistema, cameras };
+    }),
+  );
+
+  res.status(200).send(sistemasCameras);
 };
 
-export const postSistemas = (req, res) => {
+export const postSistemas = async (req, res) => {
   const { body } = req;
+  const { identificacao, numeroSerie } = body;
 
-  // TODO: implementação
-  res.status(200).send({ ...body });
+  if (!identificacao || !numeroSerie) {
+    res.status(400).send('Campos obrigatórios devem ser preenchidos');
+  }
+
+  await db.execute(
+    `insert into Sistema
+    (identificacao, numeroSerie)
+    values
+    ('${identificacao}', '${numeroSerie}')`,
+  );
+  const sistemaCadastrado = await db.getFirstResult('select id from Sistema order by id desc');
+
+  await db.execute(`insert into Camera (idSistema, principal) values (${sistemaCadastrado.id}, 0)`);
+  await db.execute(`insert into Camera (idSistema, principal) values (${sistemaCadastrado.id}, 1)`);
+
+  const camerasCadastradas = await db.getResultList(
+    `select * from Camera where idSistema = ${sistemaCadastrado.id}`,
+  );
+
+  res.status(200).send({ ...sistemaCadastrado, cameras: camerasCadastradas });
 };
